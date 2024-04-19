dde5768f937b1f14466152d00bc5513665b04f41
bdkautzman <briankautzman@gmail.com>
2024-04-19 15:33:15 -0500

    Add grid draw and grid recolor features

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const div = document.querySelector("div");
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", userInput);
+
+function userInput() {
+    let num = prompt("How many squares per side?");
+    num = Number(num);
+    if (typeof(num) !== "number"){
+        alert("Please enter a different value.")
+        return;
+    }else if (num > 100) {
+        drawBoard(100);
+    }else if (num < 0) {
+        drawBoard(1);
+    }else {
+        drawBoard(num);
+    }
+}
+
+function drawBoard(dims){
+    for(let i=0; i<dims; i++){
+        const row = document.createElement("div");
+        div.appendChild(row);
+        row.setAttribute("class", "row");
+        for(let j=0; j<dims; j++){
+            const square = document.createElement("div");
+            square.setAttribute("class", "square");
+            square.addEventListener("mouseenter", function(e){
+                square.classList.add("hovered");
+            });
+            row.appendChild(square);
+        }
+    }
+}
+
+function clearBoard(){
+    
+}
